cb2ffbdcd8228a331a23bdab48f579a9dda86e4a
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-05-29 19:42:57 +0300

    сделал функции

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const funcRandom1 = function (min, max) {
+  const random = Math.random() * (max + 1 - min) + min;
+  return Math.floor(random);
+};
+
+const funcRandom2 = function (min, max, fix) {
+  const newNumber = Math.random() * (max + 1 - min) + min;
+  return newNumber.toFixed(fix);
+};
+
+funcRandom1(0, 100);
+funcRandom2(0, 100, 2);
+
