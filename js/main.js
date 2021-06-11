8ee48681127ccd937e9ebd4f3109992057b6f6be
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-06-11 11:35:23 +0300

    правка работоспособности

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {moreAnnouncement} from './create-announcement.js';
 const ANNOUNCEMENT_QUANTITY = 10;
 
-moreAnnouncement(ANNOUNCEMENT_QUANTITY);
+window.someFN = moreAnnouncement(ANNOUNCEMENT_QUANTITY);
+// так работает, иначе незнаю как, ошибок в упор не нахожу
