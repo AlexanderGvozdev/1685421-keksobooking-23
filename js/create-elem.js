6bde796a00791f73e919e823a6d6fd23c06fda79
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-06-17 21:40:03 +0300

    на предварительную проверку

--- js/create-elem.js
+++ js/create-elem.js
@@ -5,6 +5,7 @@ const mapOffers = document.querySelector('#map-canvas');
 
 const createElement = window.someFN = createOffers(ANNOUNCEMENT_QUANTITY);
 createElement.forEach(({offer, author}) => {
+
   const popupElement = popup.cloneNode(true);
   popupElement.querySelector('.popup__title').textContent = offer.title;
   popupElement.querySelector('.popup__text--address').textContent = offer.address;
@@ -13,8 +14,31 @@ createElement.forEach(({offer, author}) => {
   popupElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   popupElement.querySelector('.popup__features').textContent = offer.features;
   popupElement.querySelector('.popup__description').textContent = offer.description;
-  popupElement.querySelector('.popup__photos');
-  popupElement.querySelector('.popup__type');
   popupElement.querySelector('.popup__avatar').src = author.avatar;
+  popupElement.querySelector('.popup__type').textContent = offer.type;
+
+  const photosList = popupElement.querySelector('.popup__photos');
+  const createPhotosList = (photos, list) => {
+    const photoListElement = popup.querySelector('.popup__photo');
+    photos.forEach((element) => {
+      const photo = photoListElement.cloneNode(true);
+      photo.src = element;
+      list.appendChild(photo);
+    });
+  };
+
+  photosList.innerHTML = '';
+  createPhotosList(offer.photos, photosList);
+
+  const removeEmptyHtmlElements = (data) => {
+    const elements = Array.from(data.children);
+    elements.forEach((element) => {
+      if (element.src === '' || element.innerHTML === '' && element.tagName.toLowerCase() !== 'img') {
+        element.remove();
+      }
+    });
+  };
   mapOffers.appendChild(popupElement);
+  removeEmptyHtmlElements(popupElement);
+
 });
