b18ebee650fa5001c4baeeaf69479a8a06fb2144
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-07-09 15:55:02 +0300

    4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-import './user-form.js';
+import {initValidation} from './user-form.js';
 import {getBlockForms} from './form-disabled.js';
 import {initMap} from './map.js';
 
 getBlockForms();
 
-initMap();
-
-/*чутка поправил, про окончания обсуждали же, что не по ТЗ,
-понимаю конечно что в будущем пригодиться - но пока так,
-с обработчиками тоже какой смысл? форма деактивирована по умолчанию и активируется при загрузке карты,
-ну если по логике, то пользователь же не сможет пользоваться нерабочей формой
- */
+initMap(initValidation());
--- js/user-form.js
+++ js/user-form.js
@@ -21,35 +21,39 @@ const housingType = document.querySelector('#type');
 const timeInFormOffer = document.querySelector('#timein');
 const timeOutFormOffer = document.querySelector('#timeout');
 
-const typeChangeHandler = () => {
-  price.placeholder = housingPrice[housingType.value];
-  price.min = housingPrice[housingType.value];
+const initValidation = () => {
+  const typeChangeHandler = () => {
+    price.placeholder = housingPrice[housingType.value];
+    price.min = housingPrice[housingType.value];
+  };
+
+  const priceValidityHandler = () => {
+    if (price.validity.valueMissing) {
+      price.setCustomValidity(`Укажите цену. Максимальная цена — ${PRICE_MAX}`);
+    } else {
+      price.setCustomValidity('');
+    }
+  };
+
+  const validateRoomsAndGuests = () => {
+    const roomNumber = roomNumberSelect.value;
+    const capacityNumber = parseInt(capacitySelect.value, 10);
+    capacitySelect.setCustomValidity(ROOM_CAPACITY[roomNumber].includes(capacityNumber) ? '' : 'Количество гостей больше чем комнат');
+  };
+
+  const onTimeChange = (sourceElement, targetElement) => {
+    if (sourceElement.value !== targetElement.value) {
+      targetElement.value = sourceElement.value;
+    }
+  };
+
+  validateRoomsAndGuests();
+  roomNumberSelect.addEventListener('change', validateRoomsAndGuests);
+  capacitySelect.addEventListener('change', validateRoomsAndGuests);
+  price.addEventListener('invalid', priceValidityHandler);
+  housingType.addEventListener('change', typeChangeHandler);
+  timeInFormOffer.addEventListener('change', onTimeChange.bind(null, timeInFormOffer, timeOutFormOffer));
+  timeOutFormOffer.addEventListener('change', onTimeChange.bind(null, timeOutFormOffer, timeInFormOffer));
 };
 
-const priceValidityHandler = () => {
-  if (price.validity.valueMissing) {
-    price.setCustomValidity(`Укажите цену. Максимальная цена — ${PRICE_MAX}`);
-  } else {
-    price.setCustomValidity('');
-  }
-};
-
-const validateRoomsAndGuests = () => {
-  const roomNumber = roomNumberSelect.value;
-  const capacityNumber = parseInt(capacitySelect.value, 10);
-  capacitySelect.setCustomValidity(ROOM_CAPACITY[roomNumber].includes(capacityNumber) ? '' : 'Количество гостей больше чем комнат');
-};
-
-const onTimeChange = (sourceElement, targetElement) => {
-  if (sourceElement.value !== targetElement.value) {
-    targetElement.value = sourceElement.value;
-  }
-};
-
-validateRoomsAndGuests();
-roomNumberSelect.addEventListener('change', validateRoomsAndGuests);
-capacitySelect.addEventListener('change', validateRoomsAndGuests);
-price.addEventListener('invalid', priceValidityHandler);
-housingType.addEventListener('change', typeChangeHandler);
-timeInFormOffer.addEventListener('change', onTimeChange.bind(null, timeInFormOffer, timeOutFormOffer));
-timeOutFormOffer.addEventListener('change', onTimeChange.bind(null, timeOutFormOffer, timeInFormOffer));
+export {initValidation};
